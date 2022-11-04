95c43b894432e9e0ca92c0f192ee7c706084f2f7
Vanessa Choe <vanessa.choe.12@gmail.com>
2022-11-04 09:54:06 +0100

    mario jumping, deleted tubes

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,15 @@
 // JavaScript Document
-console.log("Howdy!");
+console.log("Super Mario Bros!");
+
+const mario = document.querySelector('.mario');
+const tube1 = document.querySelector('.tube1');
+
+function springenNaarTube1() {
+    console.log("springenNaarTube(1)");
+    mario.classList.add("naarTube1");  
+    setTimeout(function(){
+        mario.classList.remove("naarTube1")
+    },5000);
+}
+
+tube1.addEventListener('click' , springenNaarTube1) ;
