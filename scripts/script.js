7937821c3f2217a563774e634c4682ee8426e281
Vanessa Choe <vanessa.choe.12@gmail.com>
2022-11-04 12:25:53 +0100

    changed HTMl, css and Java

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,62 @@
 // JavaScript Document
 console.log("Super Mario Bros!");
 
-const mario = document.querySelector('.mario');
-const tube1 = document.querySelector('.tube1');
+var mario1 = document.querySelector("main section ul li:nth-of-type(1) img");
+var mario2 = document.querySelector("main section ul li:nth-of-type(2) img");
+var mario3 = document.querySelector("main section ul li:nth-of-type(3) img");
+var mario4 = document.querySelector("main section ul li:nth-of-type(4) img");
+var mario5 = document.querySelector("main section ul li:nth-of-type(5) img");
+var mario6 = document.querySelector("main section ul li:nth-of-type(6) img");
+var mario7 = document.querySelector("main section ul li:nth-of-type(7) img");
+var mario8 = document.querySelector("main section ul li:nth-of-type(8) img");
+var mario9 = document.querySelector("main section ul li:nth-of-type(9) img");
+var mario10 = document.querySelector("main section ul li:nth-of-type(10) img");
+var mario11 = document.querySelector("main section ul li:nth-of-type(11) img");
 
-function springenNaarTube1() {
-    console.log("springenNaarTube(1)");
-    mario.classList.add("naarTube1");  
-    setTimeout(function(){
-        mario.classList.remove("naarTube1")
-    },5000);
-}
+var blok1 = document.querySelector("main section ul li:nth-of-type(1) button");
+var blok2 = document.querySelector("main section ul li:nth-of-type(2) button");
+var blok3 = document.querySelector("main section ul li:nth-of-type(3) button");
+var blok4 = document.querySelector("main section ul li:nth-of-type(4) button");
+var blok5 = document.querySelector("main section ul li:nth-of-type(5) button");
+var blok6 = document.querySelector("main section ul li:nth-of-type(6) button");
+var blok7 = document.querySelector("main section ul li:nth-of-type(7) button");
+var blok8 = document.querySelector("main section ul li:nth-of-type(8) button");
+var blok9 = document.querySelector("main section ul li:nth-of-type(9) button");
+var blok10 = document.querySelector("main section ul li:nth-of-type(10) button");
+var blok11 = document.querySelector("main section ul li:nth-of-type(11) button");
 
-tube1.addEventListener('click' , springenNaarTube1) ;
+
+
+blok1.addEventListener('click' , function() {
+    mario1.classList.add("naarBlok1");
+});
+blok2.addEventListener('click' , function() {
+    mario2.classList.add("naarBlok1");
+});
+blok3.addEventListener('click' , function() {
+    mario3.classList.add("naarBlok1");
+});
+blok4.addEventListener('click' , function() {
+    mario4.classList.add("naarBlok1");
+});
+blok5.addEventListener('click' , function() {
+    mario5.classList.add("naarBlok1");
+});
+blok6.addEventListener('click' , function() {
+    mario6.classList.add("naarBlok1");
+});
+blok7.addEventListener('click' , function() {
+    mario7.classList.add("naarBlok1");
+});
+blok8.addEventListener('click' , function() {
+    mario8.classList.add("naarBlok1");
+});
+blok9.addEventListener('click' , function() {
+    mario9.classList.add("naarBlok1");
+});
+blok10.addEventListener('click' , function() {
+    mario10.classList.add("naarBlok1");
+});
+blok11.addEventListener('click' , function() {
+    mario11.classList.add("naarBlok1");
+});
