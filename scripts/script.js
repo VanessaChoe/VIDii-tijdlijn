11d8e0d188c67d2ff302ee436bc77ff81492cdd7
Vanessa Choe <vanessa.choe.12@gmail.com>
2022-11-08 07:34:05 +0100

    marion jumps to the second block

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,6 @@ var blok1 = document.querySelector("main section ul li:nth-of-type(1) button");
 var marioT2 = document.querySelector("main section ul li:nth-of-type(2) img:nth-of-type(1)");
 var blok2 = document.querySelector("main section ul li:nth-of-type(2) button");
 
-
 function Mario1SpringtOmhoog() {
 
     console.log("Mario springt omhoog");
@@ -43,36 +42,83 @@ function Mario1SpringtOmhoog() {
 }
 
 function marioNaarBlok() {
+    // console.log("Mario 2 springt omhoog");
 
-    console.log("Mario 2 springt omhoog");
+    setTimeout(function(){
+        console.log("mario gaat opzij en omhoog")
+    marioT1.classList.add("marioOpzijEnOmhoog")}
+    , 500);
 
-    marioT1.classList.add("marioNaarBlok");
-    blok2.classList.add("blokjeKnippert");
-   
+    setTimeout(function(){
+    blok2.classList.add("blokjeKnippert")}
+    , 2500);
+
     setTimeout(function(){
         console.log("marioT2 verschijnt")
         marioT2.classList.add("marioVisible")}
-        , 2000);
-    
+    , 4900);
+
     setTimeout(function(){
         console.log("blok verdwijnt")
         blok2.classList.add("blokjeVerdwijnt")}
-        , 2700);
-
-    setTimeout(function(){
-        console.log("marioT2 valt naar beneden")
-        marioT2.classList.add("marioT2valt")}
-        , 4000);
+    , 5500);
 
     setTimeout(function(){
         console.log("marioT1 verdwijnt")
-        marioT1.classList.add("marioInvisible")}
-        , 4500);    
+        marioT1.classList.remove("marioVisible")}
+    , 8400); 
 
     setTimeout(function(){
         console.log("marioT2 blijft")
         marioT2.classList.add("marioT1Stays")}
-        , 4500);
+    , 8401);
+
+    
+    // setTimeout(function(){
+    //     console.log("marioT1 wordt onzichtbaar")
+    // marioT1.classList.add("marioInvisible")}
+    // , 3000);    
+    
+    // setTimeout(function(){
+    //     console.log("marioT12 wordt zichtbaar")
+    // marioT12.classList.add("omhoogSpringen")}
+    // , 5000);    
+
+    // setTimeout(function(){
+    //     console.log("marioT1 omhoog")
+    //     marioT1.classList.add("marioOmhoog")
+    //     marioT1.append('section ul li:nth-of-type(2)')
+    // }
+    // , 2000);
+
+
+    // marioT1.classList.add("marioOmhoog");
+    // blok2.classList.add("blokjeKnippert");
+   
+    // setTimeout(function(){
+    //     console.log("marioT2 verschijnt")
+    //     marioT2.classList.add("marioVisible")}
+    //     , 2000);
+    
+    // setTimeout(function(){
+    //     console.log("blok verdwijnt")
+    //     blok2.classList.add("blokjeVerdwijnt")}
+    //     , 2700);
+
+    // setTimeout(function(){
+    //     console.log("marioT2 valt naar beneden")
+    //     marioT2.classList.add("marioT2valt")}
+    //     , 4000);
+
+    // setTimeout(function(){
+    //     console.log("marioT1 verdwijnt")
+    //     marioT1.classList.add("marioInvisible")}
+    //     , 4500);    
+
+    // setTimeout(function(){
+    //     console.log("marioT2 blijft")
+    //     marioT2.classList.add("marioT1Stays")}
+    //     , 4500);
 }
 
 blok1.addEventListener("click", Mario1SpringtOmhoog);
